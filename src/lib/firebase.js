5c6178fe97cbe3650817f84c526d734de272bf8d
JonathanGrossman <jon@jonegrossman.com>
2019-12-19 05:14:50 -0600

    moved firebase config information to .env file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAnVx0KuZPUAymjo7W2nck-qn7I3SVAugk",
-  authDomain: "portfolio-641ec.firebaseapp.com",
-  databaseURL: "https://portfolio-641ec.firebaseio.com",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: "portfolio-641ec",
-  storageBucket: "portfolio-641ec.appspot.com",
-  messagingSenderId: "85057278836",
-  appId: "1:85057278836:web:d355ba84c027927d5b1221",
-  measurementId: "G-5X6D74LQR6"
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
